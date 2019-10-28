431db4a8bea65b33d51a9ec670cca6d6da35fb60
re3turn <re3turn@gmail.com>
2019-10-28 14:14:42 +0900

    Update gcd function

--- fishfiles/functions/gcd.fish
+++ fishfiles/functions/gcd.fish
@@ -1,11 +1,15 @@
 function gcd
     if not type -q (command -s ghq)
-        echo ghq command is not install
+        echo ghq command is not install 1>&2
         return 1
     end
     if test (count $argv) -ne 1
-        echo Usage: gcd repository_name
-        return 1
+        if test (ghq list | wc -l) -eq 0
+            echo ghq no repository 1>&2
+            return 2
+        end
+        cd (command ghq list --full-path | command fzf)
+        return 0
     end
     set -l GHQ (ghq root)/github.com
     set -l ME (git config --get user.name)
