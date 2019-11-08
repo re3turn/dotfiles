3d1e9e7599297277367c43bb562d81e646928db7
re3turn <re3turn@gmail.com>
2019-11-08 12:21:17 +0900

    fix duplicate set path

--- configfiles/fish/setting/command.fish
+++ configfiles/fish/setting/command.fish
@@ -1,17 +1,17 @@
 # Fisher
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME $HOME/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
 # fzf
 if not command -s fzf > /dev/null
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    $HOME/.fzf/install
 end
 set -q FZF_LEGACY_KEYBINDINGS; or set -U FZF_LEGACY_KEYBINDINGS 0
-set_u_var fish_user_paths ~/.fzf/bin
+set_u_var fish_user_paths $HOME/.fzf/bin
 
 # go
 if not command -s go > /dev/null
@@ -25,7 +25,7 @@ if not command -s go > /dev/null
         sudo apt install -y golang-go
     end
 end
-set -q GOPATH; or set -Ux GOPATH ~/.go
+set -q GOPATH; or set -Ux GOPATH $HOME/.go
 mkdir -p $GOPATH/bin
 set_u_var fish_user_paths $GOPATH/bin
 
@@ -47,6 +47,6 @@ set_u_var fish_user_paths $HOME/.nodebrew/current/bin
 
 # dot
 set -q DOT_REPO; or set -U DOT_REPO https://github.com/re3turn/dotfiles.git
-set -q DOT_DIR; or set -U DOT_DIR ~/.dotfiles
-set -q DOT_SRC; or set -U DOT_SCR ~/.dot
+set -q DOT_DIR; or set -U DOT_DIR $HOME/.dotfiles
+set -q DOT_SRC; or set -U DOT_SCR $HOME/.dot
 
