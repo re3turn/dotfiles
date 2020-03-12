acb6ba0fb998f00edd1bf8fcfd6ba0cac33c4874
re3turn <re3turn@gmail.com>
2020-03-12 16:54:37 +0900

    Add bat command

---
+++ bashfiles/command/script/bat.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -u
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+SCRIPTNAME=${0##*/}
+CMD=${SCRIPTNAME%%.*}
+
+echo "#########################################"
+echo "# Install ${CMD}"
+echo "#########################################"
+
+case ${OSTYPE} in
+    darwin* )
+        # mac
+        brew install ${CMD} 
+        ;;
+    linux* )
+        # ubuntu
+        test ${APT_UPDATE} -eq 0 && APT_UPDATE=1; sudo apt update
+        sudo apt install -y ${CMD}
+        ;;
+esac
+
