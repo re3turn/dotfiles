703d745da72ca88416296c95b4eafe17f378bdf9
re3turn <re3turn@gmail.com>
2020-05-18 09:51:58 +0900

    Add umask

--- configfiles/fish/config.fish
+++ configfiles/fish/config.fish
@@ -3,5 +3,9 @@ set FISH_ROOT ~/.config/fish
 source $FISH_ROOT/setting/environment.fish
 source $FISH_ROOT/setting/command.fish
 
+if test $OSTYPE = "linux_WSL"
+    umask 022
+end
+
 eval (dircolors -c ~/.dircolors/dircolors.256dark)
 
