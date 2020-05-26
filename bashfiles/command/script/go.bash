f29d078c2856a5f4034df7b8226fb6a9bbb4e172
re3turn <re3turn@gmail.com>
2020-05-27 03:05:57 +0900

    Fix golang install

--- bashfiles/command/script/go.bash
+++ bashfiles/command/script/go.bash
@@ -16,9 +16,12 @@ case ${OSTYPE} in
         ;;
     linux* )
         # ubuntu
+        VERSION_ID=$(cat /etc/os-release | grep VERSION_ID |  awk '$0 = substr($0, 13, 5)' | tr -d ".")
         test ${APT_UPDATE} -eq 0 && APT_UPDATE=1; sudo apt update
-        sudo apt install -y software-properties-common
-        sudo add-apt-repository -y ppa:longsleep/golang-backports
+        if [ ${VERSION_ID} -lt 2004 ]; then
+            sudo apt install -y software-properties-common
+            sudo add-apt-repository -y ppa:longsleep/golang-backports
+        fi
         sudo apt update
         sudo apt install -y golang-go
         ;;
