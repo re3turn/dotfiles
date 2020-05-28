f14a694b2aff9218234495a48a4203365e33087d
re3turn <re3turn@gmail.com>
2020-05-28 14:33:10 +0900

    Fix apt order

--- bashfiles/command/script/go.bash
+++ bashfiles/command/script/go.bash
@@ -17,15 +17,17 @@ case ${OSTYPE} in
     linux* )
         # ubuntu
         VERSION_ID=$(cat /etc/os-release | grep VERSION_ID |  awk '$0 = substr($0, 13, 5)' | tr -d ".")
-        test ${APT_UPDATE} -eq 0 && APT_UPDATE=1; sudo apt update
         if [ ${VERSION_ID} -lt 2004 ]; then
             sudo apt install -y software-properties-common
             sudo add-apt-repository -y ppa:longsleep/golang-backports
+            sudo apt update
+        else
+            test ${APT_UPDATE} -eq 0 && APT_UPDATE=1; sudo apt update
         fi
-        sudo apt update
         sudo apt install -y golang-go
         ;;
 esac
 
 # GOPATH
 mkdir -p ~/.go
+
