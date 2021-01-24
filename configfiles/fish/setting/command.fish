5f27fc3aedc8d76b37a8d627487ed50969f862aa
re3turn <re3turn@gmail.com>
2021-01-24 18:36:04 +0900

    Fisher update

--- configfiles/fish/setting/command.fish
+++ configfiles/fish/setting/command.fish
@@ -1,8 +1,8 @@
 # Fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME $HOME/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    fish -c "fisher update"
 end
 
 # fzf
