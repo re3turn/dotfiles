96bed0d2265744d56163d4de543571e197c95b34
re3turn <re3turn@gmail.com>
2021-01-27 01:08:35 +0900

    Support Arm

--- bashfiles/command/script/delta.bash
+++ bashfiles/command/script/delta.bash
@@ -4,6 +4,7 @@ set -u
 SCRIPTDIR=$(cd $(dirname $0); pwd)
 SCRIPTNAME=${0##*/}
 CMD=${SCRIPTNAME%%.*}
+source ${SCRIPTDIR}/../common/common.bash
 
 echo "#########################################"
 echo "# Install ${CMD}"
@@ -19,8 +20,9 @@ case ${OSTYPE} in
         if ! (type jq > /dev/null 2>&1); then
             bash ${SCRIPTDIR}/jq.bash
         fi
+        MACHINE=$(judge_machine)
         DELTA_RELEASE=https://api.github.com/repos/dandavison/delta/releases
-        DEB_URL=$(curl -s ${DELTA_RELEASE} | jq -r '.[0].assets[] | select(.name | test("amd64.deb")) | .browser_download_url' | head -n 1)
+        DEB_URL=$(curl -s ${DELTA_RELEASE} | jq -r ".[0].assets[] | select(.name | test(\"${MACHINE}.deb\")) | .browser_download_url" | head -n 1)
         mkdir -p ${SCRIPTDIR}/delta
         wget -qO ${SCRIPTDIR}/delta/delta.deb $DEB_URL
         sudo dpkg -i ${SCRIPTDIR}/delta/delta.deb
