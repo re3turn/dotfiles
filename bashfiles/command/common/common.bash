93101b641676100903cb638e664c3ad02d741d84
re3turn <re3turn@gmail.com>
2021-01-27 01:26:16 +0900

    Add github_release_install function

--- bashfiles/command/common/common.bash
+++ bashfiles/command/common/common.bash
@@ -8,3 +8,19 @@ judge_machine () {
     esac
 }
 
+github_release_install () {
+    if [ $# = 0 ]; then
+        echo "usage: github_release_install <github_release_url>"
+        return 1
+    fi
+    MACHINE=$(judge_machine)
+    GITHUB_RELEASE_URL=$1
+    DEB_URL=$(curl -s ${GITHUB_RELEASE_URL} | jq -r ".[0].assets[] | select(.name | test(\"${MACHINE}.deb\")) | .browser_download_url" | head -n 1)
+    local TEMPDIR=$(mktemp -d)
+    wget -qO ${TEMPDIR}/install.deb $DEB_URL
+    sudo dpkg -i ${TEMPDIR}/install.deb
+    rm -rf ${TEMPDIR}
+
+    return 0
+}
+
--- bashfiles/command/script/delta.bash
+++ bashfiles/command/script/delta.bash
@@ -20,13 +20,7 @@ case ${OSTYPE} in
         if ! (type jq > /dev/null 2>&1); then
             bash ${SCRIPTDIR}/jq.bash
         fi
-        MACHINE=$(judge_machine)
-        DELTA_RELEASE=https://api.github.com/repos/dandavison/delta/releases
-        DEB_URL=$(curl -s ${DELTA_RELEASE} | jq -r ".[0].assets[] | select(.name | test(\"${MACHINE}.deb\")) | .browser_download_url" | head -n 1)
-        mkdir -p ${SCRIPTDIR}/delta
-        wget -qO ${SCRIPTDIR}/delta/delta.deb $DEB_URL
-        sudo dpkg -i ${SCRIPTDIR}/delta/delta.deb
-        rm -rf ${SCRIPTDIR}/delta
+        github_release_install "https://api.github.com/repos/dandavison/delta/releases"
         ;;
 esac
 
