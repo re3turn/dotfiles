b8e404ed25d1b32c4f7e50d1d76cc972b43fdfd7
re3turn <re3turn@gmail.com>
2021-01-27 17:16:33 +0900

    Add make command

--- bashfiles/command/script/hw.bash
+++ bashfiles/command/script/hw.bash
@@ -18,7 +18,7 @@ case ${OSTYPE} in
     linux* )
         # ubuntu
         test ${APT_UPDATE} -eq 0 && APT_UPDATE=1; sudo apt update
-        sudo apt install -y automake google-perftools autoconf
+        sudo apt install -y automake google-perftools autoconf install build-essential
         git clone https://github.com/tkengo/highway.git ${SCRIPTDIR}/highway
         cd ${SCRIPTDIR}/highway
         ./tools/build.sh
