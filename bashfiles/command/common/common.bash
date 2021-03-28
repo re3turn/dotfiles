6f021dd2724f7ff22a5ac0b757acfb0deb0adf7e
re3turn <re3turn@gmail.com>
2021-03-28 19:57:42 +0900

    Add armv6 and armv8

--- bashfiles/command/common/common.bash
+++ bashfiles/command/common/common.bash
@@ -2,8 +2,8 @@ judge_machine () {
     local MACHINE=$(uname -m)
     case "${MACHINE}" in
         "x86_64" ) echo "amd64" ;;
-        "aarch64" ) echo "arm64" ;;
-        "armv7l" ) echo "armhf" ;;
+        "armv8" | "aarch64" ) echo "arm64" ;;
+        "armv6" | "armv7l" ) echo "armhf" ;;
         * ) echo "i386" ;;
     esac
 }
