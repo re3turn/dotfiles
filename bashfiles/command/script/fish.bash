0e368d56d6ca3d1e8b570f1c4556be0d84d50dcb
re3turn <re3turn@gmail.com>
2021-03-31 00:34:24 +0900

    Fix debian fish install

--- bashfiles/command/script/fish.bash
+++ bashfiles/command/script/fish.bash
@@ -4,6 +4,33 @@ set -u
 SCRIPTDIR=$(cd $(dirname $0); pwd)
 SCRIPTNAME=${0##*/}
 CMD=${SCRIPTNAME%%.*}
+source ${SCRIPTDIR}/../common/common.bash
+
+linux_command_install () {
+    DESTRIBUTION=$(judge_os_distribution)
+    case ${DISTRIBUTION} in
+        debian* )
+            BASE_VERSION=$(cat /etc/debian_version | awk '{print int($1)}')
+            VERSION=${BASE_VERSION}
+            if [ ${BASE_VERSION} -lt 10 ]; then
+                VERSION=9.0
+            fi
+
+            echo "deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_${VERSION}/ /" \
+                | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+            curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_${VERSION}/Release.key \
+                | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+            sudo apt update
+            sudo apt install fish
+            ;;
+        ubuntu* )
+            sudo apt-add-repository ppa:fish-shell/release-3
+            sudo apt-get update
+            sudo apt-get install fish
+            ;;
+    esac
+}
+
 
 echo "#########################################"
 echo "# Install ${CMD}"
@@ -15,10 +42,7 @@ case ${OSTYPE} in
         brew install fish
         ;;
     linux* )
-        # ubuntu
-        sudo apt-add-repository ppa:fish-shell/release-3
-        sudo apt-get update
-        sudo apt-get install fish
+        linux_command_install
         ;;
 esac
 
