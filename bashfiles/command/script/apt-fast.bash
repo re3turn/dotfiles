fde4d0cab89a7d16a1d39de24480e919bb64012c
re3turn <re3turn@gmail.com>
2021-04-09 23:02:55 +0900

    Add apt-fast

---
+++ bashfiles/command/script/apt-fast.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -u
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+SCRIPTNAME=${0##*/}
+CMD=${SCRIPTNAME%%.*}
+source ${SCRIPTDIR}/../common/common.bash
+
+linux_command_install () {
+    DISTRIBUTION=$(judge_os_distribution)
+    case ${DISTRIBUTION} in
+        debian* )
+            /bin/bash -c "$(curl -sL https://git.io/vokNn)"
+            ;;
+        ubuntu* )
+            sudo add-apt-repository ppa:apt-fast/stable
+            sudo apt update
+            sudo apt install -y apt-fast
+            ;;
+    esac
+}
+
+
+
+case ${OSTYPE} in
+    linux* )
+        echo "#########################################"
+        echo "# Install ${CMD}"
+        echo "#########################################"
+        linux_command_install
+        ;;
+esac
+
