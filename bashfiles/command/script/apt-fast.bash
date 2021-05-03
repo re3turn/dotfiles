c579e27842996d5931d447b6d3071f6bb97b5788
re3turn <re3turn@gmail.com>
2021-05-03 16:56:47 +0900

    Fix install

--- bashfiles/command/script/apt-fast.bash
+++ bashfiles/command/script/apt-fast.bash
@@ -21,9 +21,14 @@ linux_command_install () {
 }
 
 
-
 case ${OSTYPE} in
     linux* )
+        if (type /usr/sbin/${SCRIPT_NAME} > /dev/null 2>&1); then
+            exit
+        fi
+        if (type /usr/local/sbin/${SCRIPT_NAME%.*} > /dev/null 2>&1); then
+            exit
+        fi
         echo "#########################################"
         echo "# Install ${CMD}"
         echo "#########################################"
