61f2ce4544db332fde62b211e61287cc6de1ebda
re3turn <re3turn@gmail.com>
2021-08-31 16:21:55 +0900

    Sort command order

--- bashfiles/command/install.bash
+++ bashfiles/command/install.bash
@@ -4,7 +4,7 @@ set -u
 SCRIPTDIR=$(cd $(dirname $0); pwd)
 export APT_UPDATE=0
 
-for SCRIPT_PATH in $(find ${SCRIPTDIR}/script -maxdepth 1 -type f); do
+for SCRIPT_PATH in $(find ${SCRIPTDIR}/script -maxdepth 1 -type f | sort); do
     SCRIPT_NAME=$(basename ${SCRIPT_PATH})
     if (type ${SCRIPT_NAME%.*} > /dev/null 2>&1); then
         continue
