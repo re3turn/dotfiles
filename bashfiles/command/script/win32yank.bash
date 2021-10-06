2be03dfb91fdf7565ecc17bb2abcbcaedb156a2e
re3turn <re3turn@gmail.com>
2021-10-07 01:25:46 +0900

    Move install message

--- bashfiles/command/script/win32yank.bash
+++ bashfiles/command/script/win32yank.bash
@@ -5,14 +5,13 @@ SCRIPTDIR=$(cd $(dirname $0); pwd)
 SCRIPTNAME=${0##*/}
 CMD=${SCRIPTNAME%%.*}
 
-echo "#########################################"
-echo "# Install ${CMD}"
-echo "#########################################"
-
 case ${OSTYPE} in
     linux* )
         # ubuntu
         if (uname -r | grep WSL > /dev/null 2>&1); then
+            echo "#########################################"
+            echo "# Install ${CMD}"
+            echo "#########################################"
             sudo ln -fs /mnt/c/Users/${USER}/scoop/shims/win32yank.exe /usr/local/bin/win32yank.exe
             sudo ln -fs /usr/local/bin/win32yank.exe /usr/local/bin/win32yank
         fi
