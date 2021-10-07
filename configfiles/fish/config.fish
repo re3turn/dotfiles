d5554077a0ac49389cfba780c9342956e4309ec5
re3turn <re3turn@gmail.com>
2021-10-07 15:07:33 +0900

    Add local setting

--- configfiles/fish/config.fish
+++ configfiles/fish/config.fish
@@ -3,5 +3,10 @@ set FISH_ROOT ~/.config/fish
 source $FISH_ROOT/setting/environment.fish
 source $FISH_ROOT/setting/command.fish
 
+for filePath in $FISH_ROOT/setting/local/*.fish
+    source $filePath
+    echo $filePath
+end
+
 eval (dircolors -c ~/.dircolors/dircolors.256dark)
 
