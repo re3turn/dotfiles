4c3a43f36e80fe72701e7f8356cc1f1f36db548b
re3turn <re3turn@gmail.com>
2022-01-21 12:32:03 +0900

    Fix fbr argv

--- fishfiles/functions/fbr.fish
+++ fishfiles/functions/fbr.fish
@@ -1,4 +1,8 @@
 function fbr
+    if test (count $argv) -ne 0
+        git checkout $argv
+        return $status
+    end
     git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
 end
 
