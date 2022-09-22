97a940538a34bce904c4795da08fd20c18c79e81
re3turn <re3turn@gmail.com>
2022-09-22 10:36:43 +0900

    fzf preview

--- fishfiles/functions/fbr.fish
+++ fishfiles/functions/fbr.fish
@@ -3,6 +3,6 @@ function fbr
         git checkout $argv
         return $status
     end
-    git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+    git branch --all | grep -v HEAD | string trim | fzf --preview "git log -20 --color=always {}" | read -l result; and git checkout "$result"
 end
 
