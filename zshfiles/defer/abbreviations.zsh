f49941707540b6f5292cccd0ed2be1e404f0f16f
re3turn <re3turn@gmail.com>
2024-01-16 22:32:45 +0900

    Update

---
+++ zshfiles/defer/abbreviations.zsh
@@ -0,0 +1,16 @@
+########################################
+# alias 
+abbr --session mkdir='mkdir -p'
+abbr --session rm='rm -i'
+abbr --session cp='cp -i'
+abbr --session mv='mv -i'
+abbr --session ls='exa --icons --git'
+abbr --session ll='exa -l --icons --git'
+abbr --session la='exa -la --icons --git'
+abbr --session grep='rg'
+abbr --session find='fd'
+abbr --session gs='git status'
+abbr --session gc='git commit'
+abbr --session gsw='git switch'
+abbr --session gm='git merge'
+abbr --session gr='git rebase'
