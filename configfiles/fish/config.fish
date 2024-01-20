7ea09ee04ffd415adc1795dba39c8a6958ded909
re3turn <re3turn@gmail.com>
2024-01-20 23:56:56 +0900

    Add keybind

--- configfiles/fish/config.fish
+++ configfiles/fish/config.fish
@@ -3,6 +3,7 @@ set FISH_ROOT ~/.config/fish
 source $FISH_ROOT/setting/environment.fish
 source $FISH_ROOT/setting/command.fish
 source $FISH_ROOT/setting/abbreviations.fish
+source $FISH_ROOT/setting/keybind.fish
 
 for filePath in $FISH_ROOT/setting/local/*.fish
     source $filePath
