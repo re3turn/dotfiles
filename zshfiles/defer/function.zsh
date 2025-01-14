432c40ba93cde675995e086a7deb4f6a215be527
re3turn <re3turn@gmail.com>
2025-01-14 16:05:52 +0900

    Fix functions

--- zshfiles/defer/function.zsh
+++ zshfiles/defer/function.zsh
@@ -2,23 +2,42 @@
 # command
 
 ### gcd
-if [ -x "$(type -p ghq)" ]; then
-    GHQ=$(ghq root)/github.com
-    ME=$(git config --get user.name)
-
+if [ -x "$(whence ghq)" ]; then
     gcd() {
-        command ghq get -p ${1}
-        if [ $(command dirname "${1}") = "." ]; then
-            cd ${GHQ}/${ME}/${1}
+        if ! command -v ghq &> /dev/null; then
+            echo "ghq command is not installed" >&2
+            return 1
+        fi
+
+        if [ ${#} -ne 1 ]; then
+            if [ $(ghq list | wc -l) -eq 0 ]; then
+                echo "ghq no repository" >&2
+                return 2
+            fi
+            GDIR=$(ghq list --full-path | fzf)
+            if [ -n "${GDIR}" ]; then
+                cd "${GDIR}"
+            fi
+            return 0
+        fi
+
+        GHQ=$(ghq root)/github.com
+        ME=$(git config --get user.name)
+
+        ghq get -p "${1}"
+        if [ "$(dirname "${1}")" = "." ]; then
+            cd "${GHQ}/${ME}/${1}"
         else
-            cd ${GHQ}/${1}
+            cd "${GHQ}/${1}"
         fi
     }
 fi
 
 fbr() {
-    local branches branch
-    branches=$(git branch -vv) &&
-    branch=$(echo "$branches" | fzf +m) &&
-    git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+    if [ ${#} -ne 0 ]; then
+        git checkout ${1}
+        return $?
+    fi
+    branch=$(git branch --all | grep -v HEAD | sed "s/.* //" | fzf --preview "git log -20 --color=always {}") &&
+    git checkout $(echo "$branch")
 }
