406c1835f57002c2039bfb2bcd53eb3448b14ad3
re3turn <re3turn@gmail.com>
2025-03-22 16:25:45 +0900

    Use git switch

--- fishfiles/functions/fbr.fish
+++ fishfiles/functions/fbr.fish
@@ -1,8 +1,8 @@
 function fbr
     if test (count $argv) -ne 0
-        git checkout $argv
+        git switch $argv
         return $status
     end
-    git branch --all | grep -v HEAD | string trim | fzf --preview "git log -20 --color=always {}" | read -l result; and git checkout "$result"
+    git branch --all | grep -v HEAD | string trim | fzf --preview "git log -20 --color=always {}" | read -l result; and git switch "$result"
 end
 
